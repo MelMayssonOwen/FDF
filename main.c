edcbd1a470408c6aa922c935966dd61105401a10
Mel Owen <melowen@Mels-MacBook-Pro.local>
2017-07-27 00:29:36 +0200

    fast commit

--- main.c
+++ main.c
@@ -40,6 +40,7 @@ void	magic_happens(t_stor *stor, t_server *s)
 	{
 	    stor->x0 = stor->tab[z][y];
 	}
+	z++;
     }
 }
 
@@ -50,27 +51,6 @@ int	my_key_funct(int keycode, t_server	*s)//return a value with keycode blablabl
     keycode = 0;
     return (0);
 }
-
-/*
-   void		aff_tab(t_stor *stor)
-   {
-   int i;
-   int j;
-
-   i = 0;
-   while (i < 11)
-   {
-   j = 0;
-   while (j < 19)
-   {
-   printf("%d ", stor->tab[i][j]);
-   j++;
-   }
-   printf("\n");
-   i++;
-   }
-   }
-   */
 
 void		size_that_file(char **av, t_stor *stor) //give size and basic errors
 {
@@ -87,114 +67,73 @@ void		size_that_file(char **av, t_stor *stor) //give size and basic errors
 	    ft_putnerror("file not square");
 	stor->len++;
     }
-    // if (stor->nb_w != stor->len)
-    //	ft_putnerror("file not square");
     if (close(fd) != 0)
 	ft_putnerror("file didn't close properly");
 }
 
-void	aff_tab(t_stor *stor)
-{
-	for(int i = 0; i < stor->len; i++)
-	{
-		for(int j = 0; j < stor->nb_w; j++) 
-		{
-			printf("%d ", stor->tab[i][j]);
-		}
-		printf("\n");
-	}
-}
+/*
+   void	aff_tab(t_stor *stor)
+   {
+   for(int i = 0; i < stor->len; i++)
+   {
+   for(int j = 0; j < stor->nb_w; j++) 
+   {
+   printf("%d ", stor->tab[i][j]);
+   }
+   printf("\n");
+   }
+   }
+   */
 
 void		tab_create(t_stor *stor, char **av)
 {
-	char	*line;
-	int		fd;
-	int		x;
-	int		y;
-	int		i;
+    char	*line;
+    int		fd;
+    int		x;
+    int		y;
+    int		i;
 
-	int tmp;
-
-	fd = -1;
-	fd = open(av[1], O_RDONLY);
-	stor->tab = (int**)malloc(sizeof(int*) * stor->len);
-	x = -1;
-	while (get_next_line(fd, &line))
+    fd = -1;
+    fd = open(av[1], O_RDONLY);
+    stor->tab = (int**)malloc(sizeof(int*) * stor->len);
+    x = 0;
+    while (get_next_line(fd, &line))
+    {
+	stor->tab[x] = (int*)malloc(sizeof(int) * stor->nb_w);
+	y = 0;
+	i = 0;
+	while(y < stor->nb_w)
 	{
-		stor->tab[++x] = (int*)malloc(sizeof(int) * stor->nb_w);
-		y = -1;
-		i = 0;
-		while(y < stor->nb_w)
-		{
-			stor->tab[x][++y] = ft_atoi(&line[i]);
-			tmp = atoi(&line[i]);
-			printf("test i = %d // &line[i] = %d\n",i, tmp);
-			i = i + ft_strspn(&line[i], "-0123456789");
-			i++;
-			i++;
-		}
-		free(line);
+	    i += ft_strspn(&line[i], " ");
+	    stor->tab[x][y] = ft_atoi(&line[i]);
+	    y++;
+	    i += ft_strcspn(&line[i], " ");
 	}
+	x++;
+	free(line);
+    }
+    if (close(fd) != 0)
+	ft_putnerror("file didn't close properly");
+
 }
 
-/*
-void	tab_create(t_stor *stor, char **av)
-{
-	char *line;
-	int x;
-	int y;
-	int i;
-	int fd;
-
-	fd = -1;
-	fd = open(av[1], O_RDONLY);
-	stor->tab = (int**)malloc(sizeof(int*) * stor->len);
-	x = 0;
-	while (get_next_line(fd, &line))
-	{
-		i = 0;
-		y = 0;
-		while (line[i] != '\0')
-		{
-			if (line[i] == ' ')
-			{
-				i++;
-			}
-			else
-			{
-				stor->tab[x] = (int*)malloc(sizeof(int) * stor->nb_w);
-				stor->tab[x][y] = ft_atoi(&line[i]);
-				y++;
-				//printf("tab[][] = ft_atoi(&line[i])")
-				i++;
-				while (line[i] != ' ' && line[i] != '\0')
-					i++;
-			}
-		}
-		x++;
-		free(line);
-		line = NULL;
-	}
-}
-*/
 int		main(int ac, char **av)
 {
-	t_stor	*stor;
-	t_server	s[1];
+    t_stor	*stor;
+    t_server	s[1];
 
-	stor = malloc(sizeof(t_stor));
-	stor->nb_w = 0;
-	stor->len = 0;
-	if (ac != 2)
-		ft_putnerror("wrong number of file");
-	size_that_file(av, stor);
-	tab_create(stor, av);
-	printf("%dx%d\n", stor->nb_w, stor->len);
-	aff_tab(stor);
-	//s->mlx = mlx_init();
-	//s->win = mlx_new_window(s->mlx, 1000, 1000, "FDF");
-	//mlx_key_hook(s->win, my_key_funct, s);
-	//magic_happens(stor, s);
-	//mlx_loop(s->mlx);
-	return (0);
+    stor = malloc(sizeof(t_stor));
+    stor->nb_w = 0;
+    stor->len = 0;
+    if (ac != 2)
+	ft_putnerror("wrong number of file");
+    size_that_file(av, stor);
+    tab_create(stor, av);
+    //aff_tab(stor);
+    s->mlx = mlx_init();
+    s->win = mlx_new_window(s->mlx, 1000, 1000, "FDF");
+    mlx_key_hook(s->win, my_key_funct, s);
+    magic_happens(stor, s);
+    mlx_loop(s->mlx);
+    return (0);
 }
